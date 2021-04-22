368c2a37adbb01e355a0cc6293d93087683077c3
Eran Assaf <assaferan@gmail.com>
2021-04-22 18:14:41 -0400

    Update ParseNipp.cpp

--- src/ParseNipp.cpp
+++ src/ParseNipp.cpp
@@ -114,7 +114,7 @@ parseDisc(const std::string & fname, const Z & disc)
       bool done = false;
       while ((!done) && (std::getline(nippFile, line)))
 	{
-	  size_t start = line.find(find_str);
+	  size_t start = line.find(find_str.str(), 0);
 	  if (start != std::string::npos)
 	    {
 	     // found it
