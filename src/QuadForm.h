29d65fd5d0e64fbb71d0c1847695bc327d1d8c7e
Eran Assaf <assaferan@gmail.com>
2021-04-22 20:54:35 -0400

    Update QuadForm.h

--- src/QuadForm.h
+++ src/QuadForm.h
@@ -18,8 +18,8 @@ public:
     // We adhere to magma convention - giving the rows
     // up to the diagonal
     QuadForm(const RVec& coeffs);
-  
-    QuadForm(const R& a, const R& b, const R& c,
+
+    QuadForm<R, 3>(const R& a, const R& b, const R& c,
              const R& f, const R& g, const R& h)
     {
         this->a_ = a; this->b_ = b; this->c_ = c;
