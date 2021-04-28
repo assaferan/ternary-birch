782c1985e4696841ed42d67f977ebec856aa2cfa
Eran Assaf <assaferan@gmail.com>
2021-04-28 15:28:25 -0400

    Update Math.cpp

--- src/Math.cpp
+++ src/Math.cpp
@@ -208,91 +208,6 @@ std::vector< Rational<R> > Math<R>::bernoulli_poly(const size_t & n)
 // quadratic field with discrminant d (Is it? verify we are working
 // with the correct discriminant (d or 4d maybe?))
 
-template<>
-int Math<Z>::kronecker_symbol(const Z & a, const Z & n)
-{
-  // extremal cases
-  if (n == 0) return (abs(a) == 1) ? 1 : 0;
-  if (n == -1) return (a < 0) ? -1 : 1;
-  if (n == 1) return 1;
-  int two_vals[4] = {1,-1,-1,1};
-  Z idx = (a % 8) / 2;
-  size_t idx_ui = mpz_get_ui(idx.get_mpz_t());
-  if (n == 2) return (a % 2 == 0) ? 0 : two_vals[idx_ui];
-  if (a == -1) {
-    Z n_prime = n;
-    while (n_prime % 2 == 0) n_prime /= 2;
-    return ((n_prime / 2) % 2 == 0) ? 1 : -1;
-  }
-  if ((a == 2) && (n % 2 == 1)) {
-    return ((n^2 / 8) % 2 == 0) ? 1 : -1;
-  }
-  // multiplicativity
-  if (n < 0) return kronecker_symbol(a,-1)*kronecker_symbol(a,-n);
-  if (a < 0) return kronecker_symbol(-1,n)*kronecker_symbol(a,n);
-
-  // now may assume n >= 3, a >= 0
- 
-  // quadratic reciprocity
-  if (a < n) {
-    Z n_star;
-    Z n_prime = n;
-    while (n_prime % 2 == 0) n_prime /= 2;
-    n_star = ((n_prime / 2) % 2 == 0) ? n : -n;
-    return kronecker_symbol(n_star, a);
-  }
-
-  // now we may also assume a ge n
-
-  // if n = 2 mod 4, we can't reduce, use multiplicativity again
-  if (n % 4 == 2) return kronecker_symbol(a, n/2)*kronecker_symbol(a,2);
-  // now we can reduce
-  return kronecker_symbol(a % n, n);
-}
-
-template<>
-int Math<Z64>::kronecker_symbol(const Z64 & a, const Z64 & n)
-{
-  // extremal cases
-  if (n == 0) return (abs(a) == 1) ? 1 : 0;
-  if (n == -1) return (a < 0) ? -1 : 1;
-  if (n == 1) return 1;
-  int two_vals[4] = {1,-1,-1,1};
-  Z64 idx = (a % 8) / 2;
-  size_t idx_ui = idx;
-  if (n == 2) return (a % 2 == 0) ? 0 : two_vals[idx_ui];
-  if (a == -1) {
-    Z n_prime = n;
-    while (n_prime % 2 == 0) n_prime /= 2;
-    return ((n_prime / 2) % 2 == 0) ? 1 : -1;
-  }
-  if ((a == 2) && (n % 2 == 1)) {
-    return ((n^2 / 8) % 2 == 0) ? 1 : -1;
-  }
-  // multiplicativity
-  if (n < 0) return kronecker_symbol(a,-1)*kronecker_symbol(a,-n);
-  if (a < 0) return kronecker_symbol(-1,n)*kronecker_symbol(a,n);
-
-  // now may assume n >= 3, a >= 0
- 
-  // quadratic reciprocity
-  if (a < n) {
-    Z64 n_star;
-    Z64 n_prime = n;
-    while (n_prime % 2 == 0) n_prime /= 2;
-    n_star = ((n_prime / 2) % 2 == 0) ? n : -n;
-    return kronecker_symbol(n_star, a);
-  }
-
-  // now we may also assume a ge n
-
-  // if n = 2 mod 4, we can't reduce, use multiplicativity again
-  if (n % 4 == 2) return kronecker_symbol(a, n/2)*kronecker_symbol(a,2);
-  // now we can reduce
-  return kronecker_symbol(a % n, n);
-}
-
-/*
 template <typename R>
 int Math<R>::kronecker_symbol(const R & a, const R & n)
 {
@@ -300,10 +215,17 @@ int Math<R>::kronecker_symbol(const R & a, const R & n)
   if (n == 0) return (abs(a) == 1) ? 1 : 0;
   if (n == -1) return (a < 0) ? -1 : 1;
   if (n == 1) return 1;
-  int two_vals[4] = {1,-1,-1,1};
-  R idx = (a % 8) / 2;
-  size_t idx_ui(idx);
-  if (n == 2) return (a % 2 == 0) ? 0 : two_vals[idx_ui];
+  if (n == 2) {
+    if (a % 2 == 0) return 0;
+    switch((a % 8)/2) {
+    case 0:
+    case 3:
+      return 1;
+    case 1:
+    case 2:
+      return -1;
+    }
+  }
   if (a == -1) {
     R n_prime = n;
     while (n_prime % 2 == 0) n_prime /= 2;
@@ -333,7 +255,7 @@ int Math<R>::kronecker_symbol(const R & a, const R & n)
   if (n % 4 == 2) return kronecker_symbol(a, n/2)*kronecker_symbol(a,2);
   // now we can reduce
   return kronecker_symbol(a % n, n);
-  }*/
+}
 
 // B_{n. chi} where chi is the quadratic character corresponding to
 // quadratic field with discrminant d (Is it? verify we are working
