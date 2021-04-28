125db552d627fd9cbe4ef65402d01b689e6d9e80
Eran Assaf <assaferan@gmail.com>
2021-04-28 14:52:00 -0400

    Update Math.cpp

--- src/Math.cpp
+++ src/Math.cpp
@@ -218,7 +218,7 @@ int Math<Z>::kronecker_symbol(const Z & a, const Z & n)
   if (n == 1) return 1;
   int two_vals[4] = {1,-1,-1,1};
   Z idx = (a % 8) / 2;
-  size_t idx_ui = idx.mpz_get_ui();
+  size_t idx_ui = mpz_get_ui(idx.get_mpz_t());
   if (n == 2) return (a % 2 == 0) ? 0 : two_vals[idx_ui];
   if (a == -1) {
     Z n_prime = n;
