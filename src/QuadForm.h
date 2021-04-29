873f45a6561fd6c622811b7633125a338c977d8b
Eran Assaf <assaferan@gmail.com>
2021-04-28 20:46:24 -0400

    Update QuadForm.h

--- src/QuadForm.h
+++ src/QuadForm.h
@@ -218,10 +218,8 @@ class QuadFormFp_3 : public QuadFormFp<R,S,3>
 {
 public:
   using QuadFormFp<R,S,3>::QuadFormFp;
-  // using QuadFormFp<R,S,3>::discriminant;
-  // using QuadFormFp<R,S,3>::evaluate;
   using QuadFormFp<R,S,3>::field;
-  // using QuadFormFp<R,S,3>::isotropic_vector;
+  using QuadFormFp<R,S,3>::GF;
 
   R discriminant(void) const;
   R evaluate(const R&, const R&, const R&) const;
