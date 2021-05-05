b471eb2b86471472350312db519a7bd0eb1a9669
Eran Assaf <assaferan@gmail.com>
2021-05-05 17:21:33 -0400

    Update Fp.cpp

--- src/Fp.cpp
+++ src/Fp.cpp
@@ -6,7 +6,8 @@ template<>
 FpElement<W16, W32> W16_Fp::mod(const Z& a) const
 {
     Z r;
-    return FpElement(this, mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
+    return FpElement<W16,W32>(this->getptr(),
+			      mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
 }
 
 template<>
@@ -14,7 +15,8 @@ template<>
 FpElement<W32, W64> W32_Fp::mod(const Z& a) const
 {
     Z r;
-    return FpElement(this,mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
+    return FpElement<W32,W64>(this->getptr(),
+			      mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
 }
 
 template<>
@@ -22,5 +24,6 @@ template<>
 FpElement<W64, W128> W64_Fp::mod(const Z& a) const
 {
     Z r;
-    return FpElement(this,mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
+    return FpElement<W64, W128>(this->getptr(),
+				mpz_mod_ui(r.get_mpz_t(), a.get_mpz_t(), p));
 }
