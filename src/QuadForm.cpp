2c9fc354a52a09ea2be81e64680d612a0eb1cc2f
Eran Assaf <assaferan@gmail.com>
2021-05-05 17:35:31 -0400

    Update QuadForm.cpp

--- src/QuadForm.cpp
+++ src/QuadForm.cpp
@@ -71,384 +71,8 @@ Z_QuadForm<5>::get_quinary_forms(const Z & disc)
   return all_forms;
 }
 
-template<>
-Z QuadForm_Base<Z,3>::discriminant(void) const
-{
-    mpz_t disc;
-    mpz_t temp;
-    mpz_init(disc);
-    mpz_init(temp);
-
-    mpz_mul(disc, this->b_.get_mpz_t(), this->c_.get_mpz_t());      // bc
-    mpz_mul_2exp(disc, disc, 2);                                    // 4bc
-    mpz_submul(disc, this->f_.get_mpz_t(), this->f_.get_mpz_t());   // 4bc-ff
-    mpz_mul(disc, disc, this->a_.get_mpz_t());                      // 4abc-aff
-    mpz_mul(temp, this->b_.get_mpz_t(), this->g_.get_mpz_t());      // bg
-    mpz_submul(disc, temp, this->g_.get_mpz_t());                   // 4abc-aff-bgg
-    mpz_mul(temp, this->f_.get_mpz_t(), this->g_.get_mpz_t());      // fg
-    mpz_submul(temp, this->c_.get_mpz_t(), this->h_.get_mpz_t());   // fg-ch
-    mpz_addmul(disc, temp, this->h_.get_mpz_t());                   // discriminant
-
-    Z ret(disc);
-
-    mpz_clear(temp);
-    mpz_clear(disc);
-
-    return ret;
-}
-
-template<>
-bool QuadForm_Base<Z,3>::operator==(const QuadForm_Base<Z,3>& q) const
-{
-    return mpz_cmp(a_.get_mpz_t(), q.a_.get_mpz_t()) == 0 &&
-           mpz_cmp(b_.get_mpz_t(), q.b_.get_mpz_t()) == 0 &&
-           mpz_cmp(c_.get_mpz_t(), q.c_.get_mpz_t()) == 0 &&
-           mpz_cmp(f_.get_mpz_t(), q.f_.get_mpz_t()) == 0 &&
-           mpz_cmp(g_.get_mpz_t(), q.g_.get_mpz_t()) == 0 &&
-           mpz_cmp(h_.get_mpz_t(), q.h_.get_mpz_t()) == 0;
-}
-
-template<>
-Z Z_QuadForm<3>::evaluate(const Z& x, const Z& y, const Z& z) const
-{
-    mpz_t value;
-    mpz_t temp;
-    mpz_init(value);
-    mpz_init(temp);
-
-    mpz_mul(value, this->a_.get_mpz_t(), x.get_mpz_t());    // ax
-    mpz_addmul(value, this->g_.get_mpz_t(), z.get_mpz_t()); // ax+gz
-    mpz_addmul(value, this->h_.get_mpz_t(), y.get_mpz_t()); // ax+gz+hy
-    mpz_mul(value, value, x.get_mpz_t());                   // axx+gxz+hxy
-    mpz_mul(temp, this->b_.get_mpz_t(), y.get_mpz_t());     // by
-    mpz_addmul(temp, this->f_.get_mpz_t(), z.get_mpz_t());  // by+fz
-    mpz_addmul(value, temp, y.get_mpz_t());                 // axx+byy+fyz+gxz+hxy
-    mpz_mul(temp, this->c_.get_mpz_t(), z.get_mpz_t());     // cz
-    mpz_addmul(value, temp, z.get_mpz_t());                 // evaluate
-
-    Z ret(value);
-
-    mpz_clear(temp);
-    mpz_clear(value);
-
-    return ret;
-}
-
-template<>
-QuadForm<Z,3>
-QuadForm_Base<Z,3>::reduce(const QuadForm_Base<Z,3>& q, Z_Isometry<3>& s)
-//Z_QuadForm<3> Z_QuadForm<3>::reduce(const Z_QuadForm<3>& q, Z_Isometry<3>& s)
-{
-    mpz_t a, b, c, f, g, h;
-    mpz_init_set(a, q.a_.get_mpz_t());
-    mpz_init_set(b, q.b_.get_mpz_t());
-    mpz_init_set(c, q.c_.get_mpz_t());
-    mpz_init_set(f, q.f_.get_mpz_t());
-    mpz_init_set(g, q.g_.get_mpz_t());
-    mpz_init_set(h, q.h_.get_mpz_t());
-
-    // TODO: Move these into some kind of "scratch" space so that we can
-    // avoid reallocating these every time we call this function.
-    mpz_t t, num, den, temp, temp2, temp3;
-    mpz_inits(t, num, den, temp, temp2, temp3, NULL);
-
-    int flag = 1;
-    while (flag)
-    {
-        mpz_add(t, a, b);
-        mpz_add(t, t, f);
-        mpz_add(t, t, g);
-        mpz_add(t, t, h);
-        if (mpz_cmp_ui(t, 0) < 0)
-        {
-            s.A101011001();
-            mpz_add(c, c, t);
-            mpz_add(f, f, h);
-            mpz_add(f, f, b);
-            mpz_add(f, f, b);
-            mpz_add(g, g, h);
-            mpz_add(g, g, a);
-            mpz_add(g, g, a);
-        }
-
-        mpz_add(den, a, a);
-        if (mpz_cmp(a, h) >= 0)
-        {
-            mpz_sub(num, a, h);
-            mpz_fdiv_q(t, num, den);
-        }
-        else
-        {
-            mpz_add(num, a, h);
-            mpz_sub_ui(num, num, 1);
-            mpz_fdiv_q(t, num, den);
-            mpz_neg(t, t);
-        }
-        if (mpz_cmp_ui(t, 0) != 0)
-        {
-            s.A1t0010001(Z(t));
-            mpz_mul(temp, a, t);
-            mpz_add(h, h, temp);
-            mpz_addmul(b, h, t);
-            mpz_addmul(f, g, t);
-            mpz_add(h, h, temp);
-        }
-
-        mpz_add(den, b, b);
-        if (mpz_cmp(b, f) >= 0)
-        {
-            mpz_sub(num, b, f);
-            mpz_fdiv_q(t, num, den);
-        }
-        else
-        {
-            mpz_add(num, b, f);
-            mpz_sub_ui(num, num, 1);
-            mpz_fdiv_q(t, num, den);
-            mpz_neg(t, t);
-        }
-        if (mpz_cmp_ui(t, 0) != 0)
-        {
-            s.A10001t001(Z(t));
-            mpz_mul(temp, b, t);
-            mpz_add(f, f, temp);
-            mpz_addmul(c, f, t);
-            mpz_addmul(g, h, t);
-            mpz_add(f, f, temp);
-        }
-
-        mpz_add(den, a, a);
-        if (mpz_cmp(a, g) >= 0)
-        {
-            mpz_sub(num, a, g);
-            mpz_fdiv_q(t, num, den);
-        }
-        else
-        {
-            mpz_add(num, a, g);
-            mpz_sub_ui(num, num, 1);
-            mpz_fdiv_q(t, num, den);
-            mpz_neg(t, t);
-        }
-        if (mpz_cmp_ui(t, 0) != 0)
-        {
-            s.A10t010001(Z(t));
-            mpz_mul(temp, a, t);
-            mpz_add(g, g, temp);
-            mpz_addmul(c, g, t);
-            mpz_addmul(f, h, t);
-            mpz_add(g, g, temp);
-        }
-
-        if (mpz_cmp(a, b) > 0 || (mpz_cmp(a, b) == 0 && mpz_cmpabs(f, g) > 0))
-        {
-            s.A0n0n0000n();
-            mpz_swap(a, b);
-            mpz_swap(f, g);
-        }
-
-        if (mpz_cmp(b, c) > 0 || (mpz_cmp(b, c) == 0 && mpz_cmpabs(g, h) > 0))
-        {
-            s.An0000n0n0();
-            mpz_swap(b, c);
-            mpz_swap(g, h);
-        }
-
-        if (mpz_cmp(a, b) > 0 || (mpz_cmp(a, b) == 0 && mpz_cmpabs(f, g) > 0))
-        {
-            s.A0n0n0000n();
-            mpz_swap(a, b);
-            mpz_swap(f, g);
-        }
-
-        int fgh = (mpz_cmp_ui(f, 0) != 0) &&
-                  (mpz_cmp_ui(g, 0) != 0) &&
-                  (mpz_cmp_ui(h, 0) != 0);
-        if (fgh)
-        {
-            if (mpz_cmp_ui(f, 0) < 0) fgh = !fgh;
-            if (mpz_cmp_ui(g, 0) < 0) fgh = !fgh;
-            if (mpz_cmp_ui(h, 0) < 0) fgh = !fgh;
-        }
-
-        if (fgh)
-        {
-            if (mpz_cmp_ui(f, 0) < 0)
-            {
-                s.An00010001();
-                mpz_neg(f, f);
-            }
-
-            if (mpz_cmp_ui(g, 0) < 0)
-            {
-                s.A1000n0001();
-                mpz_neg(g, g);
-            }
-
-            if (mpz_cmp_ui(h, 0) < 0)
-            {
-                s.A10001000n();
-                mpz_neg(h, h);
-            }
-        }
-        else
-        {
-            int s1 = mpz_cmp_ui(f, 0) > 0;
-            int s2 = mpz_cmp_ui(g, 0) > 0;
-            int s3 = mpz_cmp_ui(h, 0) > 0;
-
-            if ((s1+s2+s3) % 2 == 1)
-            {
-                if (mpz_cmp_ui(f, 0) == 0) s1 = 1;
-                else
-                {
-                    if (mpz_cmp_ui(g, 0) == 0) s2 = 1;
-                    else if (mpz_cmp_ui(h, 0) == 0) s3 = 1;
-                }
-            }
-
-            if (s1 == 1)
-            {
-                s.An00010001();
-                mpz_neg(f, f);
-            }
-
-            if (s2 == 1)
-            {
-                s.A1000n0001();
-                mpz_neg(g, g);
-            }
-
-            if (s3 == 1)
-            {
-                s.A10001000n();
-                mpz_neg(h, h);
-            }
-        }
-
-        mpz_add(temp, a, b);
-        mpz_add(temp, temp, f);
-        mpz_add(temp, temp, g);
-        mpz_add(temp, temp, h);
-
-        flag = !(mpz_cmpabs(f, b) <= 0 && mpz_cmpabs(g, a) <= 0 &&
-                 mpz_cmpabs(h, a) <= 0 && mpz_cmp_ui(temp, 0) >= 0);
-    }
-
-    mpz_add(temp3, a, a);
-    mpz_add(temp3, temp3, h);
-    mpz_add(temp2, temp3, g);
-    mpz_add(temp2, temp2, g);
-
-    if (mpz_cmp_ui(temp, 0) == 0 && mpz_cmp_ui(temp2, 0) > 0)
-    {
-        s.An010n1001();
-        mpz_add(f, f, h);
-        mpz_add(f, f, b);
-        mpz_add(f, f, b);
-        mpz_neg(f, f);
-        mpz_add(g, g, temp3);
-        mpz_neg(g, g);
-    }
-
-    if (mpz_cmp_ui(g, 0) != 0)
-    {
-        mpz_neg(temp, h);
-        if (mpz_cmp(a, temp) == 0)
-        {
-            s.Ann00n0001();
-            mpz_add(f, f, g);
-            mpz_neg(f, f);
-            mpz_neg(g, g);
-            mpz_neg(h, h);
-        }
-    }
-
-    if (mpz_cmp_ui(h, 0) != 0)
-    {
-        mpz_neg(temp, g);
-        if (mpz_cmp(a, temp) == 0)
-        {
-            s.An0n01000n();
-            mpz_add(f, f, h);
-            mpz_neg(f, f);
-            mpz_neg(h, h);
-            mpz_add(g, g, a);
-            mpz_add(g, g, a);
-        }
-    }
-
-    if (mpz_cmp_ui(h, 0) != 0)
-    {
-        mpz_neg(temp, f);
-        if (mpz_cmp(b, temp) == 0)
-        {
-            s.A1000nn00n();
-            mpz_add(g, g, h);
-            mpz_neg(g, g);
-            mpz_neg(h, h);
-            mpz_add(f, f, b);
-            mpz_add(f, f, b);
-        }
-    }
-
-    if (mpz_cmp(a, h) == 0)
-    {
-        mpz_add(temp, f, f);
-        if (mpz_cmp(g, temp) > 0)
-        {
-            s.Ann001000n();
-            mpz_sub(f, g, f);
-        }
-    }
-
-    if (mpz_cmp(a, g) == 0)
-    {
-        mpz_add(temp, f, f);
-        if (mpz_cmp(h, temp) > 0)
-        {
-            s.An0n0n0001();
-            mpz_sub(f, h, f);
-        }
-    }
-
-    if (mpz_cmp(b, f) == 0)
-    {
-        mpz_add(temp, g, g);
-        if (mpz_cmp(h, temp) > 0)
-        {
-            s.An000nn001();
-            mpz_sub(g, h, g);
-        }
-    }
-
-    if (mpz_cmp(a, b) == 0 && mpz_cmpabs(f, g) > 0)
-    {
-        s.A0n0n0000n();
-        mpz_swap(a, b);
-        mpz_swap(f, g);
-    }
-
-    if (mpz_cmp(b, c) == 0 && mpz_cmpabs(g, h) > 0)
-    {
-        s.An0000n0n0();
-        mpz_swap(g, h);
-    }
-
-    if (mpz_cmp(a, b) == 0 && mpz_cmpabs(f, g) > 0)
-    {
-        s.A0n0n0000n();
-        mpz_swap(f, g);
-    }
-
-    Z_QuadForm<3> ret = Z_QuadForm<3>(Z(a), Z(b), Z(c), Z(f), Z(g), Z(h));
-
-    mpz_clears(a, b, c, f, g, h, t, num, den, temp, temp2, temp3, NULL);
-
-    return ret;
-}
-
-static W64 sign_vector(const Z& x, const Z& det, const std::vector<Z_PrimeSymbol>& primes)
+static W64 sign_vector(const Z& x, const Z& det,
+		       const std::vector<Z_PrimeSymbol>& primes)
 {
     W64 vec = (x == -1);
     for (const Z_PrimeSymbol& symb : primes)
@@ -488,27 +112,33 @@ template<size_t n>
 static Z_Vector<n> Z_isotropic_mod_pp(const Z_QuadForm<n>& q, const Z& p)
 {
     Z pp = p*p;
-    Z_Vector<n> vec = {0,0,1};
+    Z_Vector<n> vec;
+    vec[n-1] = 1;
 
-    // Try (0 0 1) first.
+    // Try (0 0 ... 0 1) first.
     if (q.evaluate(vec) % pp == 0)
         return vec;
 
-    // Try (0 1 x) next.
-    vec.y = 1;
-    for (vec.z = 0; vec.z < pp; vec.z++)
-        if (q.evaluate(vec) % pp == 0)
-            return vec;
-
-    // Lastly, try (1 x y).
-    vec.x = 1;
-    for (vec.y = 0; vec.y < pp; vec.y++)
-        for (vec.z = 0; vec.z < pp; vec.z++)
-            if (q.evaluate(vec) % pp == 0)
-                return vec;
-
+    // Try (0 0 .... 0 1 x) next.
+    vec[n-2] = 1;
+    for (Z x = 0; x < pp; x++) {
+      vec[n-1] = x;
+      if (q.evaluate(vec) % pp == 0)
+	return vec;
+    }
+    
+    // Lastly, try (0 0 .... 0 1 x y).
+    vec[n-3] = 1;
+    for (Z x = 0; x < pp; x++) {
+      vec[n-2] = x;
+      for (Z y = 0; y < pp; y++) {
+	vec[n-1] = y;
+	if (q.evaluate(vec) % pp == 0)
+	  return vec;
+      }
+    }
     // Otherwise, return the zero vector to indicate no solution found.
-    vec = {0};
+    for (size_t i = 0; i < n; i++) vec[i] = 0;
     return vec;
 }
 
@@ -737,12 +367,19 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
     }
 
     // Start by building a diagonal form.
+    Z_QuadForm<3>::SymVec form;
     Z a = 1;
     Z c = det;
     Z f = 0;
     Z g = 0;
     Z h = 0;
-    Z_QuadForm<3> q(a, b, c, f, g, h);
+    form[0] = 2*a;
+    form[1] = h;
+    form[2] = 2*b;
+    form[3] = f;
+    form[4] = g;
+    form[5] = 2*c;
+    Z_QuadForm<3> q(form);
     Z N = q.discriminant();
 
     // Remove the prime squares from the discriminant for those primes not
@@ -761,11 +398,11 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
             assert( q.evaluate(vec) % pp == 0 );
             #endif
 
-            if (vec.x == 0 && vec.y == 0 && vec.z == 0) break;
-            else if (vec.x == 0 && vec.y == 0)
+            if (vec[0] == 0 && vec[1] == 0 && vec[2] == 0) break;
+            else if (vec[0] == 0 && vec[1] == 0)
             {
                 #ifdef DEBUG
-                assert( vec.z == 1 );
+                assert( vec[2] == 1 );
                 assert( g % p == 0 );
                 assert( f % p == 0 );
                 assert( c % pp == 0 );
@@ -775,14 +412,14 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
                 f /= p;
                 g /= p;
             }
-            else if (vec.x == 0)
+            else if (vec[0] == 0)
             {
-                b += (c*vec.z*vec.z + f*vec.z);
-                f += (2*c*vec.z);
-                h += (g*vec.z);
+                b += (c*vec[2]*vec[2] + f*vec[2]);
+                f += (2*c*vec[2]);
+                h += (g*vec[2]);
 
                 #ifdef DEBUG
-                assert( vec.y == 1 );
+                assert( vec[1] == 1 );
                 assert( b % pp == 0 );
                 assert( f % p == 0 );
                 assert( h % p == 0 );
@@ -794,12 +431,12 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
             }
             else
             {
-                a += (b*vec.y*vec.y + c*vec.z*vec.z + f*vec.y*vec.z + g*vec.z + h*vec.y);
-                g += (2*c*vec.z + f*vec.y);
-                h += (2*b*vec.y + f*vec.z);
+                a += (b*vec[1]*vec[1] + c*vec[2]*vec[2] + f*vec[1]*vec[2] + g*vec[2] + h*vec[1]);
+                g += (2*c*vec[2] + f*vec[1]);
+                h += (2*b*vec[1] + f*vec[2]);
 
                 #ifdef DEBUG
-                assert( vec.x == 1 );
+                assert( vec[0] == 1 );
                 assert( a % pp == 0 );
                 assert( g % p == 0 );
                 assert( h % p == 0 );
@@ -809,8 +446,13 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
                 g /= p;
                 h /= p;
             }
-
-            q = Z_QuadForm<3>(a, b, c, f, g, h);
+	    form[0] = 2*a;
+	    form[1] = h;
+	    form[2] = 2*b;
+	    form[3] = f;
+	    form[4] = g;
+	    form[5] = 2*c;
+            q = Z_QuadForm<3>(form);
             N = q.discriminant();
         }
     }
@@ -824,15 +466,17 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
     {
         for (int j=3; j<=symb.power; j+=2)
         {
-            q.f_ *= symb.p;
-            q.g_ *= symb.p;
-            q.c_ *= symb.p * symb.p;
+	  q.B_(0,2) *= symb.p;
+	  q.B_(2,0) *= symb.p;
+	  q.B_(1,2) *= symb.p;
+	  q.B_(2,1) *= symb.p;
+	  q.B_(2,2) *= symb.p * symb.p;
         }
     }
     q = Z_QuadForm<3>::reduce(q, s);
 
     // Do one final verification that the symbols are correct.
-    Z x = 4 * q.a_ * q.b_ - q.h_ * q.h_;
+    Z x = q.B_(0,0) * q.B_(1,1) - q.B_(0,1) * q.B_(1,0);
     mask = 1LL << primes.size();
     for (const Z_PrimeSymbol& symb : primes)
     {
@@ -845,30 +489,4 @@ Z_QuadForm<3> Z_QuadForm<3>::get_quad_form(const std::vector<Z_PrimeSymbol>& inp
     }
 
     return q;
-}
-
-template<>
-W64 Z_QuadForm<3>::hash_value(void) const
-{
-    W64 fnv = FNV_OFFSET;
-    fnv = (fnv ^ mpz_get_si(this->a_.get_mpz_t())) * FNV_PRIME;
-    fnv = (fnv ^ mpz_get_si(this->b_.get_mpz_t())) * FNV_PRIME;
-    fnv = (fnv ^ mpz_get_si(this->c_.get_mpz_t())) * FNV_PRIME;
-    fnv = (fnv ^ mpz_get_si(this->f_.get_mpz_t())) * FNV_PRIME;
-    fnv = (fnv ^ mpz_get_si(this->g_.get_mpz_t())) * FNV_PRIME;
-    fnv = (fnv ^ mpz_get_si(this->h_.get_mpz_t())) * FNV_PRIME;
-    return fnv;
-}
-
-template<>
-W64 Z64_QuadForm<3>::hash_value(void) const
-{
-    W64 fnv = FNV_OFFSET;
-    fnv = (fnv ^ this->a_) * FNV_PRIME;
-    fnv = (fnv ^ this->b_) * FNV_PRIME;
-    fnv = (fnv ^ this->c_) * FNV_PRIME;
-    fnv = (fnv ^ this->f_) * FNV_PRIME;
-    fnv = (fnv ^ this->g_) * FNV_PRIME;
-    fnv = (fnv ^ this->h_) * FNV_PRIME;
-    return fnv;
 }
