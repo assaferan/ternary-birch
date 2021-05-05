2354ed9c91d85b5f048e3f587a8c763258dc6381
Eran Assaf <assaferan@gmail.com>
2021-05-05 18:54:01 -0400

    Update Math.h

--- src/Math.h
+++ src/Math.h
@@ -28,7 +28,7 @@ public:
   static std::vector< Rational<R> > bernoulli_poly(const size_t & n);
   static Rational<R> bernoulli_number(const size_t & n, const R & d);
 
-  static Z64 get_int(const R & a);
+  // static Z64 get_int(const R & a);
 
   static R zero();
   static R one();
