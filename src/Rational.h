de39b3da6d2a14b3e7bc0f562c10f603d7b60fcc
Eran Assaf <assaferan@gmail.com>
2021-05-05 18:20:13 -0400

    Update Rational.h

--- src/Rational.h
+++ src/Rational.h
@@ -10,9 +10,6 @@ public:
   Rational(const R& num, const R& denom) : num_(num), denom_(denom)
   { reduce(); }
 
-  Rational(const R nums[2]) : num_(nums[0]), denom_(nums[1])
-  { reduce(); }
-
   Rational(const R & num) : num_(num), denom_(1) {}
   
   // default c-tor
