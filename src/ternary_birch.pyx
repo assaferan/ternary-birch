b4784155845526e4fff731dba4a7229e6643bb05
Eran Assaf <assaferan@gmail.com>
2021-05-28 09:26:34 -0400

    Update ternary_birch.pyx

--- src/ternary_birch.pyx
+++ src/ternary_birch.pyx
@@ -174,6 +174,8 @@ cdef class BirchGenus:
         try:
             logging.info("Determining desired quadratic form")
             q = Z_QuadForm.get_quad_form(primes)
+	    tmp = q.bilinear_form()
+	    ttmp = tmp(0,0)
             a = _Z_to_int(q.bilinear_form()(0,0)) / 2
             b = _Z_to_int(q.bilinear_form()(1,1)) / 2
             c = _Z_to_int(q.bilinear_form()(2,2)) / 2
