03cc1f1e47ed8fefdce6ae600a3764bcc0610f03
Eran Assaf <assaferan@gmail.com>
2021-06-03 23:28:57 -0400

    Update birch.h

--- src/birch.h
+++ src/birch.h
@@ -260,6 +260,9 @@ using W32_VectorFp = VectorFp< W32, W64, n>;
 template<size_t n>
 using W64_VectorFp = VectorFp< W64, W128, n>;
 
+template<>
+class UnivariatePolyFp<W16,W32>;
+
 std::ostream & operator<<(std::ostream & os, const Z128 & z);
 
 #endif // __BIRCH_H_
