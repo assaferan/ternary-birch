186acf200ebb75d6ffdf3b4f91ef35091fa28b93
Eran Assaf <assaferan@gmail.com>
2021-06-07 13:06:36 -0400

    Create NumberField.h

---
+++ src/NumberField.h
@@ -0,0 +1,44 @@
+#ifndef __NUMBER_FIELD_H_
+#define __NUMBER_FIELD_H_
+
+#include "Polynomial.h"
+#include "Rational.h"
+
+template<typename R>
+class NumberFieldElement
+{
+public:
+  
+  NumberFieldElement(const UnivariatePoly< Rational<R> > & poly) : elt(poly) {}
+  NumberFieldElement(const R & a) : elt(a) {} 
+  NumberFieldElement(const Rational<R> & a) : elt(a) {}
+  
+  // arithmetic
+  NumberFieldElement<R> operator-() const;
+  NumberFieldElement<R> operator+(const NumberFieldElement<R> & ) const;
+  NumberFieldElement<R> operator-(const NumberFieldElement<R> & ) const;
+  NumberFieldElement<R> operator*(const NumberFieldElement<R> & ) const;
+  NumberFieldElement<R> operator/(const NumberFieldElement<R> & ) const;
+
+  NumberFieldElement<R> operator*(const R & ) const;
+  NumberFieldElement<R> operator/(const R & ) const;
+
+  NumberFieldElement<R> & operator+=(const NumberFieldElement<R> & );
+  NumberFieldElement<R> & operator-=(const NumberFieldElement<R> & );
+  NumberFieldElement<R> & operator*=(const NumberFieldElement<R> & );
+  NumberFieldElement<R> & operator/=(const NumberFieldElement<R> & );
+
+  NumberFieldElement<R>& operator*=(const R & );
+  NumberFieldElement<R>& operator/=(const R & );
+
+
+  static init_modulus(const Polynomial<R> & mod)
+  {this->modulus = mod; }
+  
+protected:
+  static UnivariatePoly<R> modulus;
+
+  UnivariatePoly< Rational<R> > elt;
+};
+
+#endif // _NUMBER_FIELD_H
